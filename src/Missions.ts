b1a3d48fe57b9d0f7ed5c258460b3868ea4c66a2
RichardBritoDev <richartdebrito2@gmail.com>
2025-05-13 00:03:35 -0300

    Update Missions.ts

--- src/Missions.ts
+++ src/Missions.ts
@@ -3,5 +3,9 @@ import { HeavyCargo, LightCargo, FragileCargo, Cargos } from "./Cargos";
 import { CargoShip, MiningShip, ExplorationShip, Spacecraft } from "./Spacecraft";
 
 export class Missions {
-
+    public planetsArray: Planets[] = [];
+    public cargoArray: Cargos[] = [];
+    public spacecraftArray: Spacecraft[] = [];
+    
+    
 }
